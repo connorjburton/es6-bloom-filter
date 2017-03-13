265352b0cad8e6fcda2db07e2249209e9c0a2612
Connor Burton <connorjburton@gmail.com>
2017-03-13 22:26:05 +0000

    Creating buffer with correct size

--- src/BloomFilter.js
+++ src/BloomFilter.js
@@ -1,5 +1,8 @@
 class BloomFilter {
-
+	constructor(items, input) {
+		this.input = input;
+		this.bucket = Buffer.alloc(items.length * 15); //~0.1% false positive rate
+	}
 }
 
 module.exports = BloomFilter;
