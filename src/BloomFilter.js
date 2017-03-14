31015a037ec3b0bccdeff55af8263a569c158565
Connor Burton <connorjburton@gmail.com>
2017-03-14 22:46:01 +0000

    Working on bloom filter setup

--- src/BloomFilter.js
+++ src/BloomFilter.js
@@ -1,7 +1,38 @@
+const fnv = require('fnv-plus');
+
 class BloomFilter {
-	constructor(items, input) {
+	constructor(items, input)
+	{
+		const BITS_PER_ITEM = 15; //~0.1% false positive rate
+		this.m = Buffer.alloc(items.length * BITS_PER_ITEM); // setup bit array
+		this.k = Math.ceil(BITS_PER_ITEM * 0.7); // amount of hash functions we need to use
+		this.seeds = [null]; // we don't need to seed the first hash function
 		this.input = input;
-		this.bucket = Buffer.alloc(items.length * 15); //~0.1% false positive rate
+		this.items = items;
+
+		this.setSeeds();
+		this.insertItems();
+	}
+
+	setSeeds()
+	{
+		for(let i = 0; i <= this.k; i++) this.seeds.push(Math.floor(Math.random() * 100));
+	}
+
+	insertItems()
+	{
+		console.log('Total buffer size: ' + this.m.length);
+		let collisions = 0;
+		this.items.forEach(value => {
+			this.seeds.forEach(seed => {
+				if(seed) fnv.seed(seed);
+				let index = fnv.hash(value).dec() % this.m.length;
+				if(this.m[index] === 1) collisions++;
+				this.m[index] = 1;
+			});
+		});
+
+		console.log('Total collisions: ' + collisions);
 	}
 }
 
--- src/Checker.js
+++ src/Checker.js
@@ -9,13 +9,19 @@ class Checker {
 		this.word = word;
 		this.dictionary;
 	}
-
+	
 	loadDictionary(cb)
 	{
 		fs.readFile(this.cache, 'utf8', (err, data) => { //read file
 			if(err) console.error(new Error(err));
 
-			this.dictionary = data;
+			this.dictionary = data.split('\n').map((value) => {
+				value = value.split(' ');
+				return {
+					word: value[0],
+					frequency: value[1]
+				};
+			});
 
 			cb();
 		});
@@ -24,9 +30,15 @@ class Checker {
 	run()
 	{
 		this.loadDictionary(() => {
-			let bloom = new BloomFilter();
-			console.log(this.dictionary);
+			let bloom = new BloomFilter(this.getDictionaryWords());
 		});
+	}
+
+	getDictionaryWords()
+	{
+		let arr = [];
+		this.dictionary.forEach(value => arr.push(value.word));
+		return arr;
 	}
 }
 
